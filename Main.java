a4af0341c3b5f084e733799c8dcccd5025d83c1b
ruder <deniss.ruder@ut.ee>
2023-02-07 13:57:52 +0200

    first commit

--- Main.java
+++ Main.java
@@ -1,5 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // First statement
+//        System.out.println("Hello world!");
+
+        //Decrlaration
+//        int myInt = 5;
+//
+//        myInt = myInt + 5;
+//        System.out.println(myInt);
+//
+//        System.out.println(Integer.SIZE);
+
+//        int maxInt = Integer.MAX_VALUE;
+//        int minInt = Integer.MIN_VALUE;
+
+        int underscoreInt = 1_000_000;
+//        System.out.println(maxInt);
+//        System.out.println(underscoreInt);
+//
+//        long val= 5000000000l;
+
+//        short val= 32768;
+//        short val2= 32767;
+
+//        int myint1 = 5, myint2 = 10;
+//
+//        System.out.println(myint1);
+//        System.out.println(myint2);
+
+//        byte myMinByteValue = Byte.MIN_VALUE;
+//
+//        byte myTotal= (byte) (myMinByteValue / 2);
+//
+//        System.out.println(myTotal);
+
+//        double myDouble = 10.0D;
+//        float myFloat = 10.0F;
+
+//        float myFloatValue = 5;
+//        double myDoubleValue= 5;
+//
+//        System.out.println(myFloatValue);
+//        System.out.println(myDoubleValue);
+//
+//        float myOtherFloat= (float) 5.25;
+//
+//        char myChar = 'D';
+//        char myChar2 = 68;
+//        char myChar3 = '\u0044';
+//
+//        System.out.println(myChar2);
+//        System.out.println(myChar3);
+//        System.out.println(myChar);
+
+//        boolean booleanValue1 = false;
+//        boolean booleanValue2 = true;
+
+        String myString = "Hello!" + "World!";
+        int myInt = 5;
+        myString = myString + myInt;
+        System.out.println(myString);
+
+
     }
 }
